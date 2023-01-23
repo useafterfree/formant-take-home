2e863ccba6c3e2135e6611d826099d0a38418fbf
Nathan Trujillo <nathan.d.trujillo@gmail.com>
2023-01-23 15:42:52 -0800

    Fix build

--- formant-take-home/app/robots/[action]/page.tsx
+++ formant-take-home/app/robots/[action]/page.tsx
@@ -6,28 +6,14 @@ import { RobotCard } from '@/ui/RobotCard';
 import { PageProps } from 'app/PageProps';
 
 import { z } from "zod";
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { zodResolver } from "@hookform/resolvers/zod";
-
 import { RobotSchema } from 'models/RobotSchema';
+import RobotForm from '@/ui/RobotForm';
 type FormSchemaType = z.infer<typeof RobotSchema>;
-const uiSchema = RobotSchema.omit({ id: true });
 
 type JSONResponse = {
   data?: FormSchemaType[]
   errors?: Array<{message: string}>
 }
-
-const createRobot = async (formData: z.infer<typeof uiSchema>): Promise<JSONResponse>  => {
-  try {
-    const response = await fetch('/api/robots', { method: 'POST', body: JSON.stringify(formData) });
-    const data = await response.json();
-    return { data };
-  } catch (e: any) {
-    return { errors: e };
-  }
-
-};
 
 const getRobots = async (): Promise<JSONResponse>  => {
   try {
@@ -37,88 +23,6 @@ const getRobots = async (): Promise<JSONResponse>  => {
   } catch (e: any) {
     return { errors: e };
   }
-};
-
-export const Form = () => {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<FormSchemaType>({
-    resolver: zodResolver(uiSchema), // we generate id on the api
-  });
-
-  watch();
-
-  const onSubmit: SubmitHandler<FormSchemaType> = async (formData: z.infer<typeof uiSchema>) => {
-    console.log('handler');
-    await new Promise(async (resolve) => {
-        console.log(formData);
-        try {
-          uiSchema.parse(formData);
-          const { data } = await createRobot(formData);
-          console.log(data);
-          resolve(data);
-        } catch (e) {
-          console.log(e);
-        }
-      });
-  };
-
-  return (
-    <form className="space-y-10 text-red-50" onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label className="">
-          <span className="block">Name</span>
-          <input
-            type="text"
-            className={`block border text-lg px-4 py-3 mt-2 rounded-lg border-gray-200 focus:bg-white text-gray-900 focus:border-blue-600 focus:ring-0 outline-none w-full  disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed`}
-            {...register("name")}
-            disabled={isSubmitting}
-          />
-        </label>
-        {errors.name && (
-          <p className="text-sm text-red-600 mt-1">{errors.name.message}</p>
-        )}
-      </div>
-      <div>
-        <label className="">
-          <span className="block">Arms</span>
-          <input
-            type="number"
-            className={`block border text-lg px-4 py-3 mt-2 rounded-lg border-gray-200 focus:bg-white text-gray-900 focus:border-blue-600 focus:ring-0 outline-none w-full  disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed`}
-            {...register("arms")}
-            disabled={isSubmitting}
-          />
-        </label>
-        {errors.arms && (
-          <p className="text-sm text-red-600 mt-1">{errors.arms.message}</p>
-        )}
-      </div>
-      <div>
-        <label className="">
-          <span className="block">Description</span>
-          <input
-            type="text"
-            className={`block border text-lg px-4 py-3 mt-2 rounded-lg border-gray-200 focus:bg-white text-gray-900 focus:border-blue-600 focus:ring-0 outline-none w-full  disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed`}
-            {...register("description")}
-            disabled={isSubmitting}
-          />
-        </label>
-        {errors.description && (
-          <p className="text-sm text-red-600 mt-1">{errors.description.message}ddd</p>
-        )}
-      </div>
-      <button
-        type="submit"
-        className="w-full px-8 py-4 flex items-center justify-center uppercase text-white font-semibold bg-blue-600 rounded-lg disabled:bg-gray-100 disabled:text-gray-400"
-        disabled={isSubmitting}
-      >
-        Add Robot
-      </button>
-    </form>
-  );
 };
 
 export default function Page({ params }: PageProps) {
@@ -138,7 +42,7 @@ export default function Page({ params }: PageProps) {
   const route = params.action;
   return (
     <div className="space-y-4">
-      {route === 'add' && <Form />}
+      {route === 'add' && <RobotForm />}
       {route === 'list' && robotsList.length === 0 && <div className="grid grid-cols-3 gap-6">
         {Array.from({ length: 9 }).map((_, i) => (
           <SkeletonCard key={i} />
